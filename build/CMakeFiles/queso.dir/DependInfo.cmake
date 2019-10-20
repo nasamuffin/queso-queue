3bf4c32574cde0891816471427106e42133b3928
Michael Ennis <michael@ennismightier.com>
2019-10-20 01:02:10 +0000

    Adding a level when the queue was empty resulted in confusing messages for the viewers about position.

---
+++ build/CMakeFiles/queso.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ubuntu/environment/queso-queue/chat.cpp" "/home/ubuntu/environment/queso-queue/build/CMakeFiles/queso.dir/chat.cpp.o"
+  "/home/ubuntu/environment/queso-queue/main.cpp" "/home/ubuntu/environment/queso-queue/build/CMakeFiles/queso.dir/main.cpp.o"
+  "/home/ubuntu/environment/queso-queue/quesoqueue.cpp" "/home/ubuntu/environment/queso-queue/build/CMakeFiles/queso.dir/quesoqueue.cpp.o"
+  "/home/ubuntu/environment/queso-queue/timer.cpp" "/home/ubuntu/environment/queso-queue/build/CMakeFiles/queso.dir/timer.cpp.o"
+  "/home/ubuntu/environment/queso-queue/twitch.cpp" "/home/ubuntu/environment/queso-queue/build/CMakeFiles/queso.dir/twitch.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/x86_64-linux-gnu"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/queso.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/queso.dir/main.cpp.o"
+  "CMakeFiles/queso.dir/timer.cpp.o"
+  "CMakeFiles/queso.dir/twitch.cpp.o"
+  "CMakeFiles/queso.dir/quesoqueue.cpp.o"
+  "CMakeFiles/queso.dir/chat.cpp.o"
+  "queso.pdb"
+  "queso"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/queso.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
